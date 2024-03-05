dc3e1b56f9510619817528d407bcefd339a4a91d
Duarttx <160753200+Duarttx@users.noreply.github.com>
2024-03-04 23:47:10 -0300

    Add files via upload

---
+++ Exercicio02.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Exercicio02 {
+ public static void main(String[] args) {
+   Scanner input = new Scanner(System.in);
+   Float numx;
+   Float numz;
+     System.out.print("informe um número: ");
+       numx = input.nextFloat();
+         System.out.print("Informe o segundo número: ");
+        numz = input.nextFloat();
+           System.out.print("A divisão do número é: "+ numx + " pelo número:  "+ numz + " Sendo a resposta: " + (numx / numz));
+                     input.close();
+
+ 
+  
+
+
+
+
+ }
+
+
+    
+}
